8750f779b09ab28be8eeda67ed44a6a82af57e38
HP <mehmood.memon168@gmail.com>
2019-09-05 22:14:46 +0400

    Book Rating Initial State

---
+++ book-rating/src/main/java/com/bookrating/BookRatingApplication.java
@@ -0,0 +1,13 @@
+package com.bookrating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookRatingApplication.class, args);
+	}
+
+}
